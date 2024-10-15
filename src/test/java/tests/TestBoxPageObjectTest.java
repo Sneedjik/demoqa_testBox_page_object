1cc4d362fa47b72d472ae376a34e6ae9bc5b3c65
Sergei Volkov <Sneedjik@gmail.com>
2024-10-15 19:47:04 +0600

    added TestBase

--- src/test/java/tests/TestBoxPageObjectTest.java
+++ src/test/java/tests/TestBoxPageObjectTest.java
@@ -1,21 +1,12 @@
 package tests;
 
-import com.codeborne.selenide.Configuration;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
-public class TestBoxPageObjectTest {
-
-    @BeforeAll
-    static void beforeAll() {
-        Configuration.browserSize = "1920x1080";
-        Configuration.baseUrl = "https://demoqa.com";
-        Configuration.pageLoadStrategy = "eager";
-    }
+public class TestBoxPageObjectTest extends TestBase {
 
     @Test
     void fillFormTest() {
